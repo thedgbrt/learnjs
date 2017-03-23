580113f1e3ecf7ac21d70d2fec81c933a848b3a2
Dagobert Renouf <dagobert.king@gmail.com>
2017-03-23 16:57:27 +0100

    Basic working router

--- public/app.js
+++ public/app.js
@@ -1,16 +1,31 @@
 "use strict";
 var learnjs = {};
 
-learnjs.problemView = function() {
-    return $('<div class="problem-view">').text("Coming soon!");
+learnjs.problemView = function(id) {
+    return $('<div class="problem-view">').text("Problem #" + id + " coming soon!");
 }
 
 learnjs.showView = function(hash) {
+    if(typeof(hash) === "undefined")
+        return false;
+
     var routes = {
-        "#problem-1": learnjs.problemView
+        "#problem": learnjs.problemView
     };
-    var viewFn = routes[hash];
+    
+    var params = hash.split("-");
+    
+    var viewFn = routes[params[0]];
+
     if(viewFn) {
-        $(".view-container").empty().append(viewFn());
+        $(".view-container").empty().append(viewFn(params[1]));
     }
+}
+
+learnjs.appOnReady = function() {
+    var load = function() {
+        return learnjs.showView(window.location.hash);
+    }
+    $(window).on("hashchange", load);
+    load();
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -4,7 +4,32 @@ describe('LearnJS', function() {
         expect($('.view-container .problem-view').length).toEqual(1);
     });
     it('can show the landing page when there is no hash', function() {
-        learnjs.showView();
+        learnjs.showView("");
         expect($('.view-container .landing-view').length).toEqual(1);
+    });
+    it('passes the hash view parameter to the view function', function() {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    });
+    
+    describe('Problem View', function() {
+        it('has a title that includes the problem number', function() {
+            var view = learnjs.problemView('1');
+            expect(view.text()).toEqual('Problem #1 coming soon!');  
+        });
+    });
+
+    it('invokes the router when loaded', function() {
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('subscribes to the hash change event', function() {
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger("hashchange");
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
 });
